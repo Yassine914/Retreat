9614b5321f5ea5bc5caa903ac4326503de63bca1
Yassine914 <yassine.shehab206@gmail.com>
2025-01-30 20:50:36 +0200

    yoooooooooooo. vulkan and glfw included successfuly

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
+
 int main()
 {
-    std::cout << "hello world" << std::endl;
+    std::cout << "hello world, vulkan and glfw are successfully included" << std::endl;
 }
